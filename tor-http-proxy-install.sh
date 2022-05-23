ce7dabbd6f0ccd6e2666b773d28770b49abeb2d9
buf0rd <adrianbuford@gmail.com>
2022-05-22 19:44:54 -0500

    Update tor-http-proxy-install.sh
    fixed for ufw error

--- tor-http-proxy-install.sh
+++ tor-http-proxy-install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-apt-get install tor nyx privoxy -y
+apt-get install tor nyx privoxy ufw -y
 update-rc.d privoxy enable
 update-rc.d tor enable 
+update-rc.d ufw enable 
+service ufw start
 ufw allow 9050
 ufw allow 8080
 cp ./torrc /etc/tor/torrc
